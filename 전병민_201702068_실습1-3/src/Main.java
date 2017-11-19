0c4ecfaa9e963b6fd782a8f18e921683d8435248
jeonbyeongmin <qudals7613@gmail.com>
2017-11-19 17:45:47 +0900

    first assignment

---
+++ 전병민_201702068_실습1-3/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int lineNumber;
+		
+		lineNumber = 1;
+		
+		System.out.println(lineNumber + ". 말이 되지 않는");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 그리움이 있는 줄 이제 알겠습니다.");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". ");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 말로는 나오지 않는 그리움으로");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 내 가슴은 봄빛처럼 야위어가고");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 말을 잃어버린 그리움으로");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 내 입술은 봄바람처럼 메말라갑니다.");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 이제 내 피는 그대를 향해");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". 까맣게 다 탔습니다.");
+		lineNumber = lineNumber + 1 ;
+		System.out.println(lineNumber + ". - 김용택 -");
+
+	}
+
+}

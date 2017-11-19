8d81abcac8c1425bf20f9b8ec53f0abcd10917cb
jeonbyeongmin <qudals7613@gmail.com>
2017-11-19 17:41:33 +0900

    first assignment

---
+++ 전병민_201702068_실습1-1/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("1. 나는 동일한 길을 걷고 있었네.");
+		System.out.println("2. 보도에는 깊은 구멍이 있었지.");
+		System.out.println("3. 나는 그것을 보았네.");
+		System.out.println("4. 나는 여전히 거기에 빠졌지, 습관적으로.");
+		System.out.println("5. 나의 눈은 열려 있었고,");
+		System.out.println("6. 나는 내가 어디 있는지 알았지.");
+		System.out.println("7.");
+		System.out.println("8. 나는 동일한 길을 걷고 있었네.");
+		System.out.println("9. 보도에는 깊은 구멍이 있었지.");
+		System.out.println("10. 나는 그것을 피해 돌아갔지.");
+		System.out.println("11.");
+		System.out.println("12. 나는 다른 길로 이제는 다니네.");
+		System.out.println("13.");
+		System.out.println("14. - 포티아 넬슨 - ");
+
+	}
+
+}
